d2708843fc3b4cdc1702e8e881cbc011ef1056c0
Kevin Souto <kevinsoutods@gmail.com>
2022-07-12 22:25:43 -0300

    Projeto implementando - Futuras Atualizações serão feitas

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -26,44 +26,57 @@ const Movie = () => {
         setMovie(data);
     }
 
+    const formatCurrency = (number) => {
+      return number.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      })
+    }
+
     useEffect(() => {
         const movieUrl = `${moviesURL}${id}?${apiKey}`;
         getMovie(movieUrl);
       }, []);
 
 
-    return <div>
+    return (
+      <div className="movie-page">
         {movie && 
             <>
             <MovieCard movie={movie} showLink={false} />
             <p className="tagline">{movie.tagline}</p>
             <div className="info">
               <h3>
-                <BsWallet2/>Orçamento:
+                <BsWallet2/> Orçamento:
               </h3>  
-                <p>{movie.budget}</p>
+                <p>{formatCurrency(movie.budget)}</p>
             </div>
+
             <div className="info">
               <h3>
-                <BsGraphUp/>Receita:
+                <BsGraphUp/> Receita:
               </h3>  
-                <p>{movie.revenue}</p>
+                <p>{formatCurrency(movie.revenue)}</p>
             </div>
+
             <div className="info">
               <h3>
-                <BsHourglassSplit/>Duração:
+                <BsHourglassSplit/> Duração:
               </h3>  
-                <p>{movie.runtime}</p> minutes
+                <p>{movie.runtime} minutes</p> 
             </div>
+
             <div className="info description">
               <h3>
-                <BsFillFileEarmarkTextFill/>Description:
+                <BsFillFileEarmarkTextFill/> Description:
               </h3>  
                 <p>{movie.overview}</p>
             </div>
             </>
         }
-    </div>;
+    </div>
+    );
+    
 };
 
 export default Movie;
